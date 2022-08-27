17527ced706508759b8e3427147a26ece1640c59
ADMIN <sahilbambhania@gmail.com>
2022-08-27 09:25:33 +0530

    api for archive and trash note

--- Script/dashboard.js
+++ Script/dashboard.js
@@ -1,31 +1,35 @@
+// window.addEventListener('DOMContentLoaded', () => {
 
-window.addEventListener('DOMContentLoaded', () => {
 
-    console.log("=> Connected to Dashboard.js");
-    let token = localStorage.getItem('token');
-    getAllNotes();
 
-    let navbar = document.querySelector(".side-navbar");
-    let btn = document.querySelector('#btn');
+//     console.log("=> Connected to Dashboard.js");
+    const token = localStorage.getItem('token');
+     getAllNotes();
+    
 
-    let title = document.getElementById('title');
-    let description = document.getElementById('description');
-    let color = 'Blue';
+    const navbar = document.querySelector(".side-navbar");
+    const btn = document.querySelector('#btn');
+
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const color = 'Blue';
 
     console.log(title.value);
 
-    let createnote = document.querySelector('.create-note');
-    let closebtn = document.querySelector('.close-btn');
-    let oncreate=document.querySelector('.create1');
-    let desc=document.querySelector('.create2');
+    const createnote = document.querySelector('.create-note');
+    const closebtn = document.querySelector('.close-btn');
+    const oncreate = document.querySelector('.create1');
+    const desc = document.querySelector('.create2');
 
-    let closeIcon=document.querySelector('.close-icon');
-    let serchbox=document.querySelector('.search-input');
+    const closeIcon = document.querySelector('.close-icon');
+    const serchbox = document.querySelector('.search-input');
 
-    const displaytnotes=document.querySelector('.notes');
-    const Remindernotes=document.querySelector('.Remindernotes');
-    const Archivenotes=document.querySelector('.Archivenotes');
-    const Trashnotes=document.querySelector('.Trashnotes');
+    const displaynotes = document.querySelector('.notes');
+    const Archivenotes = document.querySelector('.Archivenotes');
+    const Trashnotes = document.querySelector('.Trashnotes');
+
+    
+
 
     var noteArray;
 
@@ -33,24 +37,24 @@ window.addEventListener('DOMContentLoaded', () => {
         navbar.classList.toggle("opened");
     }
 
-    serchbox.addEventListener('focus',()=>{
+    serchbox.addEventListener('focus', () => {
         closeIcon.classList.remove('hide')
     })
-    serchbox.addEventListener('blur',()=>{
+    serchbox.addEventListener('blur', () => {
         closeIcon.classList.add('hide');
     })
 
     oncreate.addEventListener('click', () => {
-       toggleNOteFields();
+        toggleNOteFields();
     })
 
     closebtn.addEventListener('click', () => {
         let notedata = {
             title: title.value,
             description: description.value,
-            colour:color
-          }
-          console.log(notedata);
+            colour: color
+        }
+        console.log(notedata);
         $.ajax({
             url: 'https://localhost:44349/Note',
             type: 'POST',
@@ -64,7 +68,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 resetNoteFields();
                 toggleNOteFields();
                 getAllNotes();
-              },
+            },
             error: function (error) {
                 console.log(error);
                 toggleNOteFields();
@@ -72,21 +76,17 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 
-    function resetNoteFields()
-    {
-        document.getElementById('title').value='';
-        document.getElementById('description').value='';
+    function resetNoteFields() {
+        document.getElementById('title').value = '';
+        document.getElementById('description').value = '';
     }
 
-    function toggleNOteFields()
-    {
+    function toggleNOteFields() {
         createnote.classList.toggle('expand');
-        if(createnote.classList.contains('expand'))
-        {
+        if (createnote.classList.contains('expand')) {
             document.getElementById('title').placeholder = 'Title';
         }
-        else
-        {
+        else {
             document.getElementById('title').placeholder = 'Take a note...';
             resetNoteFields();
         }
@@ -102,125 +102,146 @@ window.addEventListener('DOMContentLoaded', () => {
             },
             success: function (result) {
                 console.log(result);
-                noteArray=result.data;
+                noteArray = result.data;
                 noteArray.reverse();
-              },
-              error: function (error) {
+                displaynotes.click();
+            },
+            error: function (error) {
                 console.log(error);
-              }
+            }
         })
     }
-    displaytnotes.addEventListener('click',()=>{
-        createnote.style.display = 'block'; 
-        document.getElementById('Notes').classList.add('display-notes');
-        document.getElementById('Notes').classList.remove('other-notes');
-        notes=noteArray.filter((x)=>{
-            return x.isTrash===false && x.isArchive===false;
-        });
-        console.log(notes);
-        displayAllNotes(notes);
-    })
 
+
+    function archiveNote(noteId){
+        $.ajax({
+            url:`https://localhost:44349/Note/ArchiveNote?noteId=${noteId}`,
+            type:'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            success: function (result) {
+                console.log(result);
+                getAllNotes();
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
+    }
+
+    function TrashNote(NoteId)
+    {
+        $.ajax({
+            url: `https://localhost:44349/Note/TrashNote?NoteId${NoteId}`,
+            type: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            success: function (result) {
+                console.log(result);
+                getAllNotes();
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
+    }
     
-    Remindernotes.addEventListener('click',()=>{
-        console.log(' showing reminder notes ');
-        createnote.style.display = 'block'; // sets the visibility of create-note box to hide
-        document.getElementById('Notes').classList.remove('display-notes');
-        document.getElementById('Notes').classList.add('other-notes');
-        notes=noteArray.filter((x)=>{
-            return x.isTrash===false && x.isReminder===true;
-        });
-        console.log(notes);
-        displayReminderNotes(notes);
-    })
 
-    // Event Listens when clicked on Archive in sidenav menu and calls displayAllNotes() to display Archived notes
-    Archivenotes.addEventListener('click',()=>{
-        console.log(' showing archived notes ');
-        createnote.style.display = 'none'; // sets the visibility of create-note box to hide
-        document.getElementById('Notes').classList.remove('display-notes');
-        document.getElementById('Notes').classList.add('other-notes');
-        notes=noteArray.filter((x)=>{
-            return x.isTrash===false && x.isArchive===true;
+    displaynotes.addEventListener('click', () => {
+        createnote.style.display = 'block';
+        document.getElementById('Notes').classList.add('display-notes');
+        // document.getElementById('Notes').classList.remove('other-notes');
+        notes = noteArray.filter((x) => {
+            return x.isTrash === false && x.isArchive === false;
         });
         console.log(notes);
         displayAllNotes(notes);
     })
 
-     // Event Listens when clicked on Trash in sidenav menu and calls displayAllNotes() to display Trashed notes
-     Trashnotes.addEventListener('click',()=>{
-        console.log(' showing trash notes ');
-        createnote.style.display = 'none'; // sets the visibility of create-note box to hide
+
+
+    Archivenotes.addEventListener('click', () => {
+        console.log(' showing archived notes ');
+        createnote.style.display = 'none';
         document.getElementById('Notes').classList.remove('display-notes');
         document.getElementById('Notes').classList.add('other-notes');
-        notes=noteArray.filter((x)=>{
-            return x.isTrash===true;
+        notes = noteArray.filter((x) => {
+            return x.isTrash === false && x.isArchive === true;
+        });
+        console.log(notes);
+        displayAllNotes(notes);
+    })
+
+
+    Trashnotes.addEventListener('click', () => {
+        console.log(' showing trash notes ');
+        createnote.style.display = 'none';
+        document.getElementById('Notes').classList.remove('display-notes');
+        document.getElementById('Notes').classList.add('other-notes');
+        notes = noteArray.filter((x) => {
+            return x.isTrash === true;
         });
         console.log(notes);
         displayTrashNotes(notes);
     })
 
-    
-    //function displays the filtered notearray from respective event listener using template literals to pass code dynamically
-    function displayAllNotes(Notesdata){
-       document.getElementById('Notes').innerHTML=Notesdata.map((note)=>
-       `<div class="display-div">
+
+
+    function displayAllNotes(Notesdata) {
+        document.getElementById('Notes').innerHTML = Notesdata.map((note) =>
+            `<div class="display-div">
             <div>
                 <p class="p1">${note.title}</p>
                 <P class="p2">${note.description}</P>
             </div>
             <div class="card-footer">
+            <button>
             <img src="/assets/add_alert_FILL0_wght400_GRAD0_opsz48.svg" />
+            </button>
+            <button>
             <img src="/assets/person_add_FILL0_wght400_GRAD0_opsz48.svg" />
+           </button>
+
+           <button>
             <img src="/assets/palette_FILL0_wght400_GRAD0_opsz48.svg" />
+            </button>
+           <button>
             <img src="/assets/image_FILL0_wght400_GRAD0_opsz48.svg" />
-            <img src="/assets/archive_FILL0_wght400_GRAD0_opsz48.svg" />
-            <img src="/assets/more_vert_FILL0_wght400_GRAD0_opsz48.svg" />
+            </button>
+            <button>
+            <img onclick="archiveNote(${note.noteId})" src="/assets/archive_FILL0_wght400_GRAD0_opsz48.svg" />
+            </button>
+           <button >
+            <img onclick="TrashNote(${note.noteId})" src="/assets/delete_FILL0_wght400_GRAD0_opsz48.svg" />
+           </button>
             </div>
         </div>
        `
-       ).join(' ');
+        ).join(' ');
     };
 
-    //function to display trash notes
-    function displayTrashNotes(Notesdata){
-       document.getElementById('Notes').innerHTML=Notesdata.map((note)=>
-       `<div class="display-div">
+
+    function displayTrashNotes(Notesdata) {
+        document.getElementById('Notes').innerHTML = Notesdata.map((note) =>
+            `<div class="display-div">
             <div>
                 <p class="p1">${note.title}</p>
                 <P class="p2">${note.description}</P>
             </div>
             <div class="card-footer-trash">
+            <img src="/assets/delete_forever_FILL0_wght400_GRAD0_opsz48.svg" />
+            <img src="/assets/restore_from_trash_FILL0_wght400_GRAD0_opsz48.svg" />
                 
             </div>
         </div>
        `
-       ).join(' ');
+        ).join(' ');
     };
+// })
 
-    function displayReminderNotes(Notesdata)
-    { console.log(Notesdata);
-        document.getElementById('Notes').innerHTML=Notesdata.map((note)=>
-        `<div class="display-div">
-             <div>
-                <p class="p1">${note.title}</p>
-                <P class="p2">${note.description}</P>
-            </div>
-            <div class="reminder">
-                <P class="p3">Reminder=${note.isReminder}</p>     
-            </div>
-            <div class="card-footer-trash">
-            <img src="/assets/add_alert_FILL0_wght400_GRAD0_opsz48.svg" />
-            <img src="/assets/person_add_FILL0_wght400_GRAD0_opsz48.svg" />
-            <img src="/assets/palette_FILL0_wght400_GRAD0_opsz48.svg" />
-            <img src="/assets/image_FILL0_wght400_GRAD0_opsz48.svg" />
-            <img src="/assets/archive_FILL0_wght400_GRAD0_opsz48.svg" />
-            <img src="/assets/more_vert_FILL0_wght400_GRAD0_opsz48.svg" />
-            </div>
-        </div>
-        `
-    ).join(' ');
 
-   };
-
-})
+   
